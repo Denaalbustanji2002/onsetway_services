e5bba919fae811fc1389047a28fa8f9927d8cbcc
Osetway <osetway@192.168.1.225>
2025-08-26 13:40:39 +0300

    new

---
+++ lib/cubit/auth_cubit.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AuthCubit extends StatefulWidget {
+  const AuthCubit({super.key});
+
+  @override
+  State<AuthCubit> createState() => _AuthCubitState();
+}
+
+class _AuthCubitState extends State<AuthCubit> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/state/auth_state.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AuthState extends StatefulWidget {
+  const AuthState({super.key});
+
+  @override
+  State<AuthState> createState() => _AuthStateState();
+}
+
+class _AuthStateState extends State<AuthState> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
